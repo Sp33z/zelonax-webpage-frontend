332e4195583a8f1f446a91ac5d5474e12757c2af
Dominik Kürti <xdominik.kurti@gmail.com>
2024-09-25 23:22:42 +0200

    Fixed responsive view of the menu in mobile landscape.

--- src/components/Navbars/MobileNavbar.jsx
+++ src/components/Navbars/MobileNavbar.jsx
@@ -6,20 +6,20 @@ function MobileNavbar(props)
     let currentPage = window.location.pathname;
     let isMenuOpened = props.isMenuOpened;
 
-    return <div className = {"absolute top-0 left-0 h-full w-3/4 bg-gray-400 md:hidden shadow-md shadow-white transition duration-700 " + (isMenuOpened ? "": "-translate-x-full")}>
+    return <div className = {"absolute top-0 left-0 h-full w-3/4 md:w-1/2 bg-gray-400 xl:hidden shadow-md shadow-white transition duration-700 " + (isMenuOpened ? "": "-translate-x-full")}>
         <div className="flex flex-col h-full w-full py-8 px-4">
-            <Link className="w-fit h-fit text-3xl font-bold">Zelonax</Link>
-            <div className="flex flex-col my-auto">
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "/" ? "": "opacity-50") }>Home</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Services</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Fleet</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Request a Quote</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>About Us</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Careers</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>News</Link>
-                <Link className= { "bg-black text-white p-3 my-1 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Our Partners</Link>
+            <Link className="w-fit h-fit text-3xl font-bold mb-3">Zelonax</Link>
+            <div className="flex flex-col my-auto overflow-y-auto">
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "/" ? "": "opacity-50") }>Home</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Services</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Fleet</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Request a Quote</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>About Us</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Careers</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>News</Link>
+                <Link className= { "bg-black text-white p-2 my-2 font-semibold transition duration-300 rounded-xl hover:opacity-100 " + (currentPage == "Services" ? "": "opacity-50") }>Our Partners</Link>
             </div>
-            <p className="mt-auto text-sm">All rights resered. © 2024 Zelonax.</p>
+            <p className="my-auto text-sm mt-3">All rights resered. <br/> © 2024 Zelonax.</p>
         </div>
     </div>
 }
